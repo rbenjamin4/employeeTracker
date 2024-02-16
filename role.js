a6b2a4d008f6662790544933877261f66c4986ef
Reed <reedbenjamin4@gmail.com>
2024-02-16 14:28:18 -0500

    Completed functionality to add role to database

--- role.js
+++ role.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer')
 const sequelize = require('./config/connection')
 require('console.table')
 
@@ -12,8 +13,40 @@ const viewDepartment = async(start) => {
     return result[0]
 }
 
-const addRole = async() => {
+const addRole = async(start) => {
     const allDepartments = await viewDepartment()
+    console.table(allDepartments)
+    let deptPrompt = []
+    for(let i = 0; i < allDepartments.length; i++){
+        const object = {
+            name: allDepartments[i].name,
+            value: allDepartments[i].id
+        }
+        deptPrompt.push(object)
+    }
+    console.table(deptPrompt)
+    const response = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Select the department:',
+            name: 'department',
+            choices: deptPrompt
+        },
+        {
+            type: 'input',
+            message: 'Enter the title of the role:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Enter the salary for the role:',
+            name: 'salary'
+        }
+    ]).then(async ({department, title, salary}) => {
+        const result = await sequelize.query(`INSERT INTO role (title, salary, department_id) VALUES ('${title}', '${salary}', ${department});`)
+        console.table(result)
+        start()
+    })
 }
 
 module.exports = {
